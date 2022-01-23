0e0f55acae2d7da29247f843b39dd909b1f6e76a
magic <magician19841125@gmail.com>
2022-01-24 01:26:31 +0800

    Remove OSDQueue.kt : It unuseful Add CallbackHandler.kt : Ensure that callback events are in the main thread.

--- LibOsd/src/main/java/idv/bruce/ui/osd/view/OSDGlSurfaceView.kt
+++ LibOsd/src/main/java/idv/bruce/ui/osd/view/OSDGlSurfaceView.kt
@@ -12,39 +12,40 @@ import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 
-class OSDGlSurfaceView(context: Context, attrs: AttributeSet) : GLSurfaceView(context, attrs),
-    OsdView<GLES30> {
-    private lateinit var surface: Surface
+class OSDGlSurfaceView(context : Context, attrs : AttributeSet) : GLSurfaceView(context, attrs),
+                                                                  OsdView<GLES30> {
+    private lateinit var surface : Surface
 
-    private var isVideoSurfaceUpdated: Boolean = false
+    private var isVideoSurfaceUpdated : Boolean = false
 
     init {
+        TODO("This view is not implement yet")
         setRenderer(PlayRenderer())
     }
 
-    override fun addOsdItem(item: OSDItem<GLES30>) {
+    override fun addOsdItem(item : OSDItem<GLES30>) {
 
     }
 
-    override fun removeOsdItem(item: OSDItem<GLES30>) {
+    override fun removeOsdItem(item : OSDItem<GLES30>) {
 
     }
 
     private class PlayRenderer : Renderer {
-        override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        override fun onSurfaceCreated(gl : GL10?, config : EGLConfig?) {
 
         }
 
-        override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        override fun onSurfaceChanged(gl : GL10?, width : Int, height : Int) {
 
         }
 
-        override fun onDrawFrame(gl: GL10?) {
+        override fun onDrawFrame(gl : GL10?) {
 
         }
     }
 
-    val onFrameAvailableLister: SurfaceTexture.OnFrameAvailableListener =
+    val onFrameAvailableLister : SurfaceTexture.OnFrameAvailableListener =
         SurfaceTexture.OnFrameAvailableListener {
             isVideoSurfaceUpdated = true
             requestRender()
